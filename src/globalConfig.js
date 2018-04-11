e7d1a70daafa82a47b4e13be3d58ae06b7183fd0
bluesky12 <18811363400@139.com>
2018-04-11 15:53:42 +0800

    Navigator

--- src/globalConfig.js
+++ src/globalConfig.js
@@ -1,6 +1,8 @@
 import wepy from 'wepy';
 import {installGlobalProps} from 'fancy-mini/lib/wepyKit';
 import AdaptiveToast from 'fancy-mini/lib/AdaptiveToast';
+import Navigator from 'fancy-mini/lib/navigate/Navigator';
+import {customWxPromisify} from 'fancy-mini/lib/wxPromise';
 
 let adaptiveToast = new AdaptiveToast({
   icons: {
@@ -10,3 +12,12 @@ let adaptiveToast = new AdaptiveToast({
 });
 installGlobalProps(adaptiveToast); //installGlobalProps(adaptiveToast, adaptiveToast.installProps);
 
+
+Navigator.install();
+installGlobalProps("$wxPromise", customWxPromisify({
+  overrides: {
+    navigateTo: Navigator.navigateTo,
+    redirectTo: Navigator.redirectTo,
+    navigateBack: Navigator.navigateBack,
+  }
+}));
